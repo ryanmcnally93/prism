72e4e267bb9e1658f384cac50751ea61359a1823
McNallyR <McNallyR@192.168.1.125>
2024-07-11 09:04:20 +0100

    Sort Fames

--- src/main/java/com/sapient/sapient/Callouts/CalloutController.java
+++ src/main/java/com/sapient/sapient/Callouts/CalloutController.java
@@ -29,6 +29,7 @@ public class CalloutController {
 	
 	@GetMapping("/home")
 	public String home(Model model) {
+		// Pass the HTML blank instances to work with
 		Certificates cert = new Certificates();
 		model.addAttribute("cert", cert);
 		Experiences exp = new Experiences();
@@ -37,11 +38,17 @@ public class CalloutController {
 		model.addAttribute("fame", fame);
 		Callout newC = new Callout();
 		model.addAttribute("callout", newC);
-		model.addAttribute("fames", famesrepository.findAll());
+		// This organises all the 'callouts' by time and reverses them
 		List<Callout> sortedCallouts = calloutRepository.findAll();
 		Collections.sort(sortedCallouts, Comparator.comparing(Callout::getCalloutDate).reversed());
+		// This organises all the 'fames' by time and reverses them
+		System.out.println(famesrepository.findAll());
+		List<Fames> sortedFames = famesrepository.findAll();
+		Collections.sort(sortedFames, Comparator.comparing(Fames::getFameDate).reversed());
+		System.out.println(sortedFames);
+		// Pass fames and callouts to HTML
 		model.addAttribute("callouts", sortedCallouts);
-		
+		model.addAttribute("fames", sortedFames);
 		return "index";
 	}
 	
