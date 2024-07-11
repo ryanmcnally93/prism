8e288dce31b8e6890c92887c56d69be01903ad9e
McNallyR <McNallyR@192.168.1.125>
2024-07-11 14:16:49 +0100

    not sure

--- src/main/java/com/sapient/sapient/Callouts/CalloutController.java
+++ src/main/java/com/sapient/sapient/Callouts/CalloutController.java
@@ -2,7 +2,6 @@ package com.sapient.sapient.Callouts;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.sapient.sapient.HallOfFame.FameRepository;
 import com.sapient.sapient.HallOfFame.FameService;
 import com.sapient.sapient.HallOfFame.Fames;
