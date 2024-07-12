0d38fa61a46307ef403bd09746da3a27039cb323
Mary Chacia <marykchacia@gmail.com>
2024-07-12 12:33:24 +0100

    Delete src/main/SmallImprovements directory

--- src/main/SmallImprovements/SmallImprovementsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sapient.sapient.SmallImprovements;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SmallImprovementsRepository extends JpaRepository<SmallImprovements, Integer> {
-}
