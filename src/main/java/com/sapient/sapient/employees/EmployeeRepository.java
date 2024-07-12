f9d8bee03284bca70294c45d1eafbee569d5a83b
JoshuaDaryl <135657835+JoshuaDaryl@users.noreply.github.com>
2024-07-12 13:47:27 +0100

    Delete src/main/java/com/sapient/sapient/employees directory

--- src/main/java/com/sapient/sapient/employees/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sapient.employees;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository <Employee, Integer> {
-
-}
