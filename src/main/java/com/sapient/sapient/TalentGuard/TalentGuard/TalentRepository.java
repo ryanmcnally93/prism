314d34e94d09c05c05a134e8f5c330f49dea4950
Ryan McNally <120190010+ryanmcnally93@users.noreply.github.com>
2024-07-12 09:46:46 +0100

    Delete src/main/java/com/sapient/sapient/TalentGuard directory

--- src/main/java/com/sapient/sapient/TalentGuard/TalentGuard/TalentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sapient.sapient.TalentGuard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jakarta.transaction.Transactional;
-
-public interface TalentRepository extends JpaRepository<TalentGuards, Integer> {
-	
-}
